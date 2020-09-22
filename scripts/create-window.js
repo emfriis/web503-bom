408bb8718f56470e26688052f5aed36aaae416e2
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-09-22 12:46:27 +1200

    Removed comments from base scripts

--- scripts/create-window.js
+++ scripts/create-window.js
@@ -1,21 +1,10 @@
 function createWindow() {
     var win = window.open("", "My Window", "width=500, height=200,screenX=100,screenY=100");
-    // window properties
     var isclose = win.closed;
     var name = win.name;
-    // writing in the current document
     document.write(isclose + "<br>");
     document.write(name + "<br>");
     document.write(win.screenY + "<br>");
     document.write(win.screenX + "<br>");
-    // we can access the new window document like this
     win.document.write("Hello World!");
 }
-
-/**
- * In the example above, we have created a new window, just to make you familiar with new window creation and also,
-to make you understand that when we create a new window, then the current window has a different window object
-and the new window will have a separate window object.
-
-* We have then tried to access some properties of the new window created.
- */
